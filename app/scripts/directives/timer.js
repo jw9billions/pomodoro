a802af5ccc9a1da59b7c6600b692e87ed28884bd
Jane Wang <jw9billions@gmail.com>
2017-12-27 23:56:31 -0800

    Start and Reset timer

---
+++ app/scripts/directives/timer.js
@@ -0,0 +1,40 @@
+(function() {
+  function timer ($interval) {
+
+    return {
+      templateUrl: '/templates/directives/timer.html',
+      replace: true,
+      restrict: 'E',
+      scope: { },
+      link: function(scope, elemnt, attibutes) {
+
+        var interval, incrementTimer, actions;
+        actions = { start: "Start", stop: "Stop" };
+
+        scope.workTimer = 1500;
+        scope.action = actions.start;
+
+        incrementTimer = function() {
+          scope.workTimer -- ;
+        };
+
+        scope.toggle = function () {
+          if (scope.action == actions.start) {
+            scope.action = actions.stop;
+            interval = $interval(incrementTimer, 1000, 1500);
+            console.log(interval)
+          } else if (scope.action == actions.stop) {
+            scope.action = actions.start;
+            $interval.cancel(interval);
+            scope.workTimer = 1500;
+          }
+        };
+      }
+    };
+
+  }
+
+  angular
+    .module('pomodoroTime')
+    .directive('timer', timer);
+})();
